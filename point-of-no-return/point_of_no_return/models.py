9e21c22d3ecf4ed1562e37a16147a7b19fbe2015
anthonymang <anthonymanganielloda@gmail.com>
2021-06-30 14:42:19 -0400

    tune up models

--- point-of-no-return/point_of_no_return/models.py
+++ point-of-no-return/point_of_no_return/models.py
@@ -12,16 +12,14 @@ release_types = (
 )
 
 class Artist(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=50)
-    bio = models.CharField(max_length=1000)
-    bandcamp_link = models.URLField()
-    spotify_link = models.URLField()
-    instagram_link = models.URLField()
-    twitter_link = models.URLField()
-    discogs_link = models.URLField()
-    spotify_embed = models.CharField(max_length=1000)
-    bandcamp_embed = models.CharField(max_length=1000)
+    name = models.CharField(blank =True, max_length=100)
+    location = models.CharField(blank =True, max_length=50)
+    bio = models.CharField(blank =True, max_length=1000)
+    bandcamp_link = models.URLField(blank =True )
+    spotify_link = models.URLField(blank =True )
+    instagram_link = models.URLField(blank =True )
+    discogs_link = models.URLField(blank =True )
+    spotify_embed = models.CharField(blank =True, max_length=1000)
     tags = TaggableManager()
 
     def __str__(self):
@@ -30,44 +28,45 @@ class Artist(models.Model):
 
 
 class Curator(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(blank =True, max_length=100)
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     
     def __str__(self):
         return self.name
 
 class Comment(models.Model):
-    content = models.CharField(max_length=1000)
+    content = models.CharField(blank =True, max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class Submission(models.Model):
-    artist_name = models.CharField(max_length=100)
-    link = models.URLField()
-    comments = models.CharField(max_length=1000)
-    social_media_link = models.URLField()
-    you = models.CharField(max_length=3, choices = (
+    artist_name = models.CharField(blank =True, max_length=100)
+    link = models.URLField(blank =True )
+    comments = models.CharField(blank =True, max_length=1000)
+    social_media_link = models.URLField(blank =True )
+    you = models.CharField(blank =True, max_length=3, choices = (
         ('Yes', 'Yes'),
         ('No', 'No')
     ))
 
 class Music(models.Model):
-    name = models.CharField(max_length=100)
-    release_year = models.IntegerField()
-    release_date = models.DateField()
-    description = models.CharField(max_length=1000)
-    spotify_embed = models.CharField(max_length=1000)
-    bandcamp_embed = models.CharField(max_length=1000)
-    discogs_link = models.URLField()
-    genre = models.CharField(max_length=1000)
-    release_type = models.CharField(max_length=10, choices = release_types)
+    name = models.CharField(blank =True, max_length=100)
+    release_year = models.IntegerField(blank =True)
+    release_date = models.DateField(blank =True)
+    released_by = models.CharField(blank =True, max_length=1000)
+    description = models.CharField(blank =True, max_length=1000)
+    spotify_embed = models.CharField(blank =True, max_length=1000)
+    bandcamp_embed = models.CharField(blank =True, max_length=1000)
+    discogs_link = models.URLField(blank =True )
+    genre = models.CharField(blank =True, max_length=1000)
+    release_type = models.CharField(blank =True, max_length=10, choices = release_types)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     curator = models.ForeignKey(Curator, on_delete=models.CASCADE)
     artist = models.ManyToManyField(Artist)
-    comments = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    comments = models.ForeignKey(Comment, null = True, blank=True, on_delete=models.CASCADE)
     tags = TaggableManager()
 
 
     def __str__(self):
-        return self.name
+        return (f'{self.name}, {self.release_year}')
 

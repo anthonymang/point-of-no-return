4cf4e1950832f45a71bc36f9d0b06ed400a2bf5f
anthonymang <anthonymanganielloda@gmail.com>
2021-07-02 16:45:38 -0400

    edit settings

--- point-of-no-return/mysite/settings.py
+++ point-of-no-return/mysite/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'point-of-no-return.point_of_no_return',
+    'point-of-no-return/point_of_no_return',
     'taggit'
 ]
 
